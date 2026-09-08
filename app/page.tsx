a4a9ed405c4d9aa2e59e55ae4b696e61815f0cd5
Sebastian <sebastiantanpersonal@gmail.com>
2026-09-07 17:36:25 -0700

    Fix desktop bio spacing and line wrapping

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { aboutItems } from "@/lib/home"
 export default function Home() {
   return (
     <PageShell>
-      <div className="mt-5 space-y-4 sm:mt-6 sm:space-y-4">
+      <div className="mt-5 space-y-4 sm:mt-6 sm:space-y-5 sm:[&_p]:leading-[1.6] sm:[&_p]:text-pretty">
         {aboutItems.map((item, i) => (
           <Paragraph key={i}>
             <span dangerouslySetInnerHTML={{ __html: item.html }} />
